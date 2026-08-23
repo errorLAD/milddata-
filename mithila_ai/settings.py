20860c2ec5e1361cb3bf718e3e270d6f52d65ebd
Abhishek Mishra <ab.mishra@yahoo.com>
2026-08-23 11:52:37 +0530

    Update settings.py

--- mithila_ai/settings.py
+++ mithila_ai/settings.py
@@ -71,9 +71,8 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "Asia/Kolkata"
 USE_I18N = True
 USE_TZ = True
-
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
